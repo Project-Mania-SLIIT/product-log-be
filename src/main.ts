a859c1dc888a4dfee105c0e31d509fb8dc6111b1
Pasindu Lakshan Gunasekara <shanugunasekara11@gmail.com>
2024-05-11 09:36:07 +0530

    change origin url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://dev--product-log.netlify.app'],
+    origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   });
